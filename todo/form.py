fd4e9c47b8cec2ce99b5b2da9f0805b638e718cf
tanvir0188 <arnob0188@gmail.com>
2025-10-25 15:41:36 +0600

    update code

--- todo/form.py
+++ todo/form.py
@@ -5,6 +5,19 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields= "__all__"
+        widgets = {
+            "title": forms.TextInput(attrs={
+                'class': 'form-input',
+                'placeholder': 'Enter your title'
+            }),
+            "describtion": forms.TextInput(attrs={
+                'class': 'form-text',
+                'placeholder': 'Enter your description'
+            }),
+            "image": forms.ClearableFileInput(attrs={
+                'class': 'form-input',
+            }),
+        }
         
                      
                               
